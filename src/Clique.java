833d2237d27e53689541486c1619b4d6b777e0b6
ayalgul <afghanijewman@gmail.com>
2015-07-26 03:22:49 -0400

    added k tournament component to genetic algorithm clique problem

--- src/Clique.java
+++ src/Clique.java
@@ -1,11 +1,11 @@
 
-
+import java.util.*;
 
 public class Clique{
 	
 	
-	
-	
+
+// checks if the chromosome is 	a clique
 public static boolean check(int []comb, int[][] clique)	
 {
 	int k=comb.length; 
@@ -34,8 +34,105 @@ public static boolean check(int []comb, int[][] clique)
 	return true; 
 }
 
+//checks the fitness value of each chromosome by setting the 
+//total fitness value to one and checking each possible 
+//edge (ith and jth pair). if that edge does not exist
+// or numbers are duplicates we subtract from total fitness. 
+//then we divide the total fitness to the max fitness. 
+public static float fitnessCheck(int []comb, int[][] clique)	
+{
+	int k=comb.length; 
+	float fitness= k*(k-1);
+	int i=0; 
+	
+	while(i<k)
+	{
+		int j=0; 
+		while(j<k)
+		{
+			if(i!=j)
+			{
+				if(comb[i]==comb[j])
+					fitness--; 
+				else if(clique[comb[i]][comb[j]]==0)
+					fitness--;
+				
+			}
+			j++; 
+		}
+		i++;
+	}
+	
+	return fitness/(k*(k-1)); 
+}
 
-public static void bruteClique(int[][]  clique, int K){
+public static void geneticClique(int [][]clique, int k, int popsize, int maxgen, int fitthresh, int tournsize)
+{
+	
+	int n= clique.length; 
+	int population[][]= new int[popsize][k];
+	float fitness[]= new float[popsize];
+	
+	for(int j=0; j<popsize; j++)
+	{
+		int array[] = new int [k]; 
+		for(int i=0; i<k; i++)
+			array[i]= (int) (Math.random()*n)+0;
+		
+		fitness[j]=fitnessCheck(array, clique);
+		population[j]=array; 
+	}
+	
+	int t=0; 
+	int ktourn[][] = new int[tournsize][k];
+	boolean tourncheck[]= new boolean[popsize]; 
+	
+	
+		
+		while(t<tournsize)
+		{
+			int r=(int) (Math.random()*(popsize))+0;
+			if(tourncheck[r]==false)
+			{
+			ktourn[t]= population[r]; 
+				
+					System.out.println(r+" "); 
+				
+			t++;
+			tourncheck[r]=true;
+			}
+		}
+		
+		
+		
+	
+}
+
+
+// just generates a random possible solution and checks if that solution is
+// acceptable or not. 
+public static void randomClique(int [][] clique, int k)
+{
+	int array[] = new int [k]; 
+	int n= clique.length; 
+	
+	for(int i=0; i<k; i++)
+			array[i]= (int) (Math.random()*n)+0;
+			
+	
+	if(check(array, clique))
+	{
+		System.out.println("Solution found");
+		for(int i=0; i<k; i++)
+				System.out.print(array[i]+" ");
+		
+	}
+
+}
+
+//checks all possible solutions of the clique problem by using a combination
+//algorithm I got from my friend Nate Ritholtz a former queens college student. 
+public static void bruteClique(int[][]  clique, int k){
  
        
         int n = clique.length;
@@ -46,12 +143,12 @@ public static void bruteClique(int[][]  clique, int K){
         	elements[i]=i; 
         }
          
-        if(K > n){
+        if(k > n){
             System.out.println("Invalid input, K > N");
             return;
         }
        
-        int combination[] = new int[K];
+        int combination[] = new int[k];
          
        
         int r = 0;      
@@ -59,10 +156,10 @@ public static void bruteClique(int[][]  clique, int K){
          
         while(r >= 0){
             
-            if(index <= (n + (r - K))){
+            if(index <= (n + (r - k))){
                     combination[r] = index;
                      
-                if(r == K-1){
+                if(r == k-1){
   
                 	if(check(combination, clique))
             		{
@@ -99,7 +196,7 @@ public static void bruteClique(int[][]  clique, int K){
 				          {1,1,1,1,1},
 				          {0,1,1,0,1}};	
 
-		bruteClique(clique,3);
+		geneticClique(clique,3, 1000, 100, 2 ,50);
 	}
 
 }
